24858ba7b761d884239363c8c1c6cd8e7ecb1c67
AbdelQuinonez <ahiran.quinonez@gmail.com>
2023-01-10 14:05:22 -0700

    added hilt config

---
+++ app/src/main/java/com/example/applaudoscodechallengeandroid/ApplaudosApplication.kt
@@ -0,0 +1,7 @@
+package com.example.applaudoscodechallengeandroid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ApplaudosApplication : Application()
