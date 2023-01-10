6e17d159fa8ad33c34a4fb1d21d9d3e9e1038689
AbdelQuinonez <ahiran.quinonez@gmail.com>
2023-01-10 13:02:10 -0700

    added colors and types

---
+++ app/src/main/java/com/example/applaudoscodechallengeandroid/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.example.applaudoscodechallengeandroid.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primaryColor = Color(0xFF6243FF)
+val secondaryColor = Color(0xFF0013CA)
+
+//Tertiary
+val tertiaryColor = Color(0xFF21BDCA)
+val tertiaryVariant = Color(0xFF84DFE2)
+val tertiarySecondaryVariant = Color(0xFF32B4FF)
+
+//Neutrals
+val BLACK = Color(0xFF25294A)
+val BACKGROUND = Color(0xFFF0F2F5)
+val WHITE = Color(0xFFFBFAFE)
+val TEXT = Color(0xFF6B6B83)
+val SUBTLE_TEXT = Color(0xFF8C8CA1)
+val GRAY = Color(0xFFD5D8DB)
+
+//Others
+
+val GRADIENT = Color(0xFF6243FF)
+val ERROR = Color(0xFFF65164)
